6ada93591e99d1f2cbe500263677244953955b9e
ju <tjdals9938@naver.com>
2022-06-09 18:00:50 +0900

    [feat] StudyController class add .

---
+++ src/main/java/juniper/local/juniper/controller/StudyController.java
@@ -0,0 +1,15 @@
+package juniper.local.juniper.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/test")
+public class StudyController {
+
+
+}
