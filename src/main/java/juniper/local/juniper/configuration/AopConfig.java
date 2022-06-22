67903232aa44c4c5059af4bae34b8c342a58a4e1
ju <tjdals9938@naver.com>
2022-06-22 10:50:27 +0900

    [feat]: AOP add .

---
+++ src/main/java/juniper/local/juniper/configuration/AopConfig.java
@@ -0,0 +1,31 @@
+package juniper.local.juniper.configuration;
+
+import juniper.local.juniper.vo.Response;
+import juniper.local.juniper.vo.Result;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class AopConfig {
+
+    @AfterReturning(value = "execution(* juniper.local.juniper.controller.*.*(..))", returning = "result")
+    public Object AfterReturning(Object result) {
+
+        if (result != null) {
+            if(result instanceof Result) {
+                ((Result)result).setToken("test");
+            } else if (result instanceof Response) {
+                ((Response)result).getResponse().setToken("test");
+            } else if (result instanceof ResponseEntity){
+                Response response = (Response)((ResponseEntity)result).getBody();
+                response.getResponse().setToken("test");
+            }
+        }
+
+        return result;
+    }
+
+}
